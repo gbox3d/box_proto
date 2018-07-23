8e599eda4164f5c510f5f24c2fcac5aea269fb04
gbox3d <gbox3d@gmail.com>
2018-07-24 00:58:10 +0900

    ie11을 위한 js문법 버전 낮추기

--- home/test_ui_handlers.js
+++ home/test_ui_handlers.js
@@ -1,6 +1,9 @@
 function lm_test_ui_hander() {
 
+  console.log('test ui load');
   let ui_root = document.querySelector("#test-ui");
+
+  console.log(ui_root)
 
   ui_root.querySelector("button.add-text").addEventListener('click',function () {
     theApp.ObjectMgr.addText("hello",0,0,'Calibri','#000000',60);
